a184aeb4408cc6a640bdd77a206d6733365562f0
Xinyi Wei <wxy205073@outlook.com>
2024-02-17 11:33:51 -0500

    Start JS

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
