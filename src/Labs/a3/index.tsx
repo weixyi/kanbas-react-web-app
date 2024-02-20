d00b9f3bad1760d2ad908690bc37d40a1cf2c0fe
Xinyi Wei <wxy205073@outlook.com>
2024-02-20 16:46:11 -0500

    Finish 2.3

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,17 +1,24 @@
 import JavaScript from "./JavaScript";
 import {Route, Routes} from "react-router-dom";
 import Add2 from "./routing/Add2";
+import Add from "./Add";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
 
 function Assignment3() {
     return (
         <div>
             <Routes>
-                <Route path="add/:a/:b" element={<Add2 />} />
+                <Route path="add/:a/:b" element={<Add2/>}/>
                 {/* Other routes for Assignment3 can be added here */}
             </Routes>
             <h1>Assignment 3</h1>
+            <TodoList/>
+            <TodoItem/>
+            <Add a={33} b={4}/>
             <JavaScript/>
         </div>
     );
 }
+
 export default Assignment3;
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -2,10 +2,27 @@ import Assignment3 from "./a3";
 import {Link, Route, Routes} from "react-router-dom";
 import Nav from "../Nav";
 import PathParameters from "./a3/routing/PathParameters";
+import Classes from "./a3/Classes";
+import Styles from "./a3/Styles";
+import ConditionalOutput from "./a3/ConditionalOutput";
+import Highlight from "./a3/Hightlight";
 function Labs() {
     return(
         <div>
             <Nav/>
+            <h1>Assignment 3</h1>
+            <h1>Highlight</h1>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+            vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+            <Highlight>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+            </Highlight>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+            vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+            <ConditionalOutput/>
+            <Styles/>
+            <Classes/>
             <PathParameters/>
             <Routes>
                 <Route path="/a3/*" element={<Assignment3 />} />
