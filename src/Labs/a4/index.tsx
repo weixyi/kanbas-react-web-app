5a14734947a8af4607cf7add59a410f758704787
Xinyi Wei <wxy205073@outlook.com>
2024-03-18 16:23:34 -0400

    2.4

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import ClickEvent from "./ClickEvent";
 import PassingDataOnEvent from "./PassingDataOnEvent";
 import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+import Counter from "./Counter"
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 const Assignment4 = () => {
     function sayHello(){
         alert("Hello")
@@ -10,6 +18,14 @@ const Assignment4 = () => {
     return(
         <>
             <h1>Labs</h1>
+            <ParentStateComponent/>
+            <ArrayStateVariable/>
+            <ObjectStateVariable/>
+            <DateStateVariable/>
+            <StringStateVariables/>
+            <BooleanStateVariables/>
+            <Counter/>
+            <EventObject/>
             <ClickEvent/>
             <PassingDataOnEvent/>
             <PassingFunctions theFunction={sayHello}/>
